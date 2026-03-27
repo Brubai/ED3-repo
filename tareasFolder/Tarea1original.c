522f47d5a7feed803426f6ad7fe0c45a1979b98e
Bruno Augusto Baiutti <106946353+Brubai@users.noreply.github.com>
2026-03-27 10:41:00 -0300

    Refactor Tarea1original.c for clarity and functionality

--- tareasFolder/Tarea1original.c
+++ tareasFolder/Tarea1original.c
@@ -4,11 +4,9 @@
 #include <stdio.h>
 #include <cr_section_macros.h>
 
-int zbrun;
-// TODO: insert other include files here
-void delay();
-// TODO: insert other definitions and declarations here
+int volatile zbrun;
 
+void delay();
 int main(void) {
 
 
@@ -19,45 +17,42 @@ int main(void) {
 	        { 0, -1, 1},
 	        { 1, 0, -1},
 	        {-1, 1, 0}};
-
 	int cata=0;
-    LPC_PINCON -> PINSEL0 &= ~(0xFFF);
-
+    LPC_PINCON -> PINSEL0 &= ~(0x3FFF);
     LPC_PINCON -> PINMODE0 |= (0x3F);
-
     LPC_GPIO0 -> FIODIR &= ~(0x7);
     LPC_GPIO0 -> FIODIR |= (0b111 << 4);
 
     while(1)
     {
-    	cata++;
-    	int random= cata%3;
     	do{
     		zbrun = LPC_GPIO0-> FIOPIN & (0b111);
+			cata++;
+			int random= cata%3;
     	}while(zbrun==0);
     	bruno=mapa[zbrun];
     	if(bruno==-1){
     		continue;
     	}
-    emi= resultado[bruno][random];
-    if(emi==1){
-    	LPC_GPIO0 -> FIOSET = (1 << 5);
-    	delay();
-    	LPC_GPIO0 -> FIOCLR = (1 << 5);
-    }
-    if (emi==0){
-    	LPC_GPIO0 -> FIOSET = (1 << 4);
-    	delay();
-    	LPC_GPIO0 -> FIOCLR = (1<<4);
-    }
-    if(emi==-1){
-    	LPC_GPIO0 -> FIOSET = (1<< 6);
-    	delay();
-    	LPC_GPIO0 -> FIOCLR = (1 <<6);
-    }
-    printf("Jugada usuario:%d | Jugada CPU:%d | Resultado: %d \n",bruno,random,emi  );
-    while((LPC_GPIO0->FIOPIN & 0b111) != 0);
-    }
+		emi= resultado[bruno][random];
+		if(emi==1){
+			LPC_GPIO0 -> FIOSET = (1 << 5);
+			delay();
+			LPC_GPIO0 -> FIOCLR = (1 << 5);
+		}
+		if (emi==0){
+			LPC_GPIO0 -> FIOSET = (1 << 4);
+			delay();
+			LPC_GPIO0 -> FIOCLR = (1<<4);
+		}
+		if(emi==-1){
+			LPC_GPIO0 -> FIOSET = (1<< 6);
+			delay();
+			LPC_GPIO0 -> FIOCLR = (1 <<6);
+		}
+		printf("Jugada usuario:%d | Jugada CPU:%d | Resultado: %d \n",bruno,random,emi  );
+		while((LPC_GPIO0->FIOPIN & 0b111) != 0);
+		}
 
     return 0 ;
 }
